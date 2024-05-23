4d722c94f8ce0d4ed8292d675d4ac95a1d292aaa
laowang1992 <wangpf0608@126.com>
2024-05-23 10:49:01 +0800

    add timeout in epcr.pl

--- 05.Primer/epcr.pl
+++ 05.Primer/epcr.pl
@@ -1,62 +1,108 @@
 #!/bin/env perl
-# Date: 2022-01-13
-# Usage: perl epcr.pl xxx.p3out
+# Date: 2024-05-23
+# Usage: perl epcr.pl xxx.p3out output.txt
 
 use strict;
 use warnings;
 
-open IN, "$ARGV[0]";
-open OUT, ">$ARGV[1]";
+open my $p3out_fh, '<', $ARGV[0] or die "Cannot open $ARGV[0]: $!";
+open my $output, '>', $ARGV[1] or die "Cannot open $ARGV[1]: $!";
 
-my $id;
-my $num;
-my $primerL;
-my $primerR;
-my @epcr;
-my $hit;
-my $tmL;
-my $tmR;
-my $gcL;
-my $gcR;
-my $len;
+# 设置超时时间
+my $timeout = 2;
 
-print OUT "ID	NUM	PrimerL	PrimerR	Hit	TmL	TmR	GCL	GCR	Length\n";
-
-while(<IN>){
-	chomp;
-	if(/^SEQUENCE_ID=(.+)$/){
-		$id = $1;
-	}
-	if(/PRIMER_LEFT_(\d)_SEQUENCE=(.+)/){
-		# 是否需要清空变量?
-		$num = $1;
-		$primerL = $2;
-	}
-	if(/PRIMER_RIGHT_\d_SEQUENCE=(.+)/){
-		$primerR = $1;
-		# 执行e-pcr，并计算返回结果有多少行;
-		@epcr = readpipe("re-PCR -s genome.hash -n 1 -g 1 $primerL $primerR 50-1000");
-		$hit = @epcr;
-		$hit -= 2;
-	}	
-	if(/PRIMER_LEFT_\d_TM=(.+)/){
-		$tmL = $1;
-	}
-	if(/PRIMER_RIGHT_\d_TM=(.+)/){
-		$tmR = $1;
-	}
-	if(/PRIMER_LEFT_\d_GC_PERCENT=(.+)/){
-		$gcL = $1;
-	}
-	if(/PRIMER_RIGHT_\d_GC_PERCENT=(.+)/){
-		$gcR = $1;
-	}
-	if(/PRIMER_PAIR_\d_PRODUCT_SIZE=(.+)/){
-		$len = $1;
-		print OUT "$id\t$num\t$primerL\t$primerR\t$hit\t$tmL\t$tmR\t$gcL\t$gcR\t$len\n"
+print $output "ID	NUM	PrimerL	PrimerR	Hit	TmL	TmR	GCL	GCR	Length\n";
+while (my %primers = get_next_primers($p3out_fh)) {
+	for (my $i = 1; $i <= $primers{'number'}; ++$i) {
+		my $id      = $primers{'id'};
+		my $num     = $i -1;
+		my $primerL = @{$primers{'p_left'}}[$num];
+		my $primerR = @{$primers{'p_right'}}[$num];
+		my $tmL     = @{$primers{'tm_left'}}[$num];
+		my $tmR     = @{$primers{'tm_right'}}[$num];
+		my $gcL     = @{$primers{'gc_left'}}[$num];
+		my $gcR     = @{$primers{'gc_right'}}[$num];
+		my $len     = @{$primers{'size'}}[$num];
+		my @epcr;
+		my $hit;
+		eval {
+			# 设置alarm，超时时间为$timeout秒
+			local $SIG{ALRM} = sub {die "timeout\n"};
+			alarm $timeout;
+			
+			# 执行外部命令
+			@epcr = readpipe("re-PCR -s genome.hash -n 1 -g 1 $primerL $primerR 50-1000");
+			
+			# 取消alarm
+			alarm 0;
+		};
+		
+		if ($@) {
+			if ($@ eq "timeout\n") {
+				print "$id $num : e_PCR command timed out, skipping this iteration\n";
+				$hit = "timeout";
+				#next;  # 跳过当前循环，进入下一个循环
+			} else {
+				die $@;  # 处理其他可能的异常
+			}
+		} else {
+			$hit = @epcr;
+			$hit -= 2;
+		}
+		
+		print $output "$id\t$num\t$primerL\t$primerR\t$hit\t$tmL\t$tmR\t$gcL\t$gcR\t$len\n"
+		
 	}
 }
 
-close IN;
-close OUT;
+close $p3out_fh;
+close $output;
+
+sub get_next_primers {
+	my ($fh) = @_;
+	my %primers = (
+		'p_left'    => [],
+		'p_right'   => [],
+		'tm_left'   => [],
+		'tm_right'  => [],
+		'gc_left'   => [],
+		'gc_right'  => [],
+		'size'      => [],
+	);
+	while (my $line = <$fh>) {
+		chomp $line;
+		# SEQUENCE_ID不能有空格
+		if ($line =~ /^SEQUENCE_ID=(.+)$/) {
+			$primers{'id'} = $1;
+		}
+		if ($line =~ /^PRIMER_LEFT_NUM_RETURNED=(\d+)$/) {
+			$primers{'number'} = $1;
+		}
+		if ($line =~ /^PRIMER_LEFT_\d+_SEQUENCE=(.+)$/){
+			push @{$primers{'p_left'}}, $1;
+		}
+		if ($line =~ /^PRIMER_RIGHT_\d+_SEQUENCE=(.+)$/){
+			push @{$primers{'p_right'}}, $1;
+		}
+		if ($line =~ /^PRIMER_LEFT_\d+_TM=(.+)$/) {
+			push @{$primers{'tm_left'}}, $1;
+		}
+		if ($line =~ /^PRIMER_RIGHT_\d+_TM=(.+)$/) {
+			push @{$primers{'tm_right'}}, $1;
+		}
+		if ($line =~ /^PRIMER_LEFT_\d+_GC_PERCENT=(.+)$/) {
+			push @{$primers{'gc_left'}}, $1;
+		}
+		if ($line =~ /^PRIMER_RIGHT_\d+_GC_PERCENT=(.+)$/) {
+			push @{$primers{'gc_right'}}, $1;
+		}
+		if ($line =~ /^PRIMER_PAIR_\d+_PRODUCT_SIZE=(.+)$/) {
+			push @{$primers{'size'}}, $1;
+		}
+		if ($line =~ /^=$/) {
+			return %primers;
+		}
+	}
+	return ();
+}
 
